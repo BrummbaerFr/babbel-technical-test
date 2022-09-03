8da0d2365cc20d0feb19d395af19296db1df5e65
BrummbaerFr <richard.eric.blondel@gmail.com>
2022-09-03 09:08:52 +0200

    Added return types for repo methods

--- src/repositories/language.repository.ts
+++ src/repositories/language.repository.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { ILanguage } from '../models/ILanguage';
-import { Connection, ResultSetHeader, RowDataPacket } from 'mysql2/promise';
+import { Connection, FieldPacket, ResultSetHeader } from 'mysql2/promise';
 
 class LanguageRepository {
 	private connection: Promise<Connection>;
@@ -9,23 +9,23 @@ class LanguageRepository {
 		this.connection = connection;
 	}
 
-	public async getLanguages() {
+	public async getLanguages(): Promise<[ILanguage[], FieldPacket[]]> {
 		return (await this.connection).query<ILanguage[]>('SELECT * FROM languages');
 	}
 
-	public async addLanguage(name: string, code: string) {
+	public async addLanguage(name: string, code: string): Promise<[ResultSetHeader, FieldPacket[]]> {
 		return (await this.connection).execute<ResultSetHeader>('INSERT INTO languages (name, code) VALUES (?, ?)', [name, code]);
 	}
 
-	public async updateLanguage(name: string, newCode: string, code: string) {
+	public async updateLanguage(name: string, newCode: string, code: string): Promise<[ResultSetHeader, FieldPacket[]]> {
 		return (await this.connection).execute<ResultSetHeader>('UPDATE languages SET name = ?, code = ? WHERE code = ?', [name, newCode, code]);
 	}
 
-	public async deleteLanguages() {
+	public async deleteLanguages(): Promise<[ResultSetHeader, FieldPacket[]]> {
 		return (await this.connection).query<ResultSetHeader>('TRUNCATE TABLE languages');
 	}
 
-	public async deleteLanguage(code: string) {
+	public async deleteLanguage(code: string): Promise<[ResultSetHeader, FieldPacket[]]> {
 		return (await this.connection).execute<ResultSetHeader>('DELETE FROM languages WHERE code = ?', [ code ]);
 	}
 }
--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { IUser } from '../models/IUser';
-import { Connection, ResultSetHeader, RowDataPacket } from 'mysql2/promise';
+import { Connection, ResultSetHeader, FieldPacket } from 'mysql2/promise';
 
 class UserRepository {
 	private connection: Promise<Connection>;
@@ -9,21 +9,21 @@ class UserRepository {
 		this.connection = connection;
 	}
 
-	public async createAccount(firstName: string, lastName: string, username: string, password: string) {
+	public async createAccount(firstName: string, lastName: string, username: string, password: string): Promise<[ResultSetHeader, FieldPacket[]]> {
 		return (await this.connection).execute<ResultSetHeader>('INSERT INTO users (first_name, last_name, username, password) VALUES (?, ?, ?, ?)',
 			[firstName, lastName, username, password]);
 	}
 
-	public async getAccount(id: number) {
+	public async getAccount(id: number): Promise<[IUser[], FieldPacket[]]> {
 		return (await this.connection).execute<IUser[]>('SELECT * FROM users WHERE id = ?', [id]);
 	}
 
-	public async updateAccount(id: number, firstName: string, lastName: string, username: string, password: string) {
+	public async updateAccount(id: number, firstName: string, lastName: string, username: string, password: string): Promise<[ResultSetHeader, FieldPacket[]]> {
 		return (await this.connection).execute<ResultSetHeader>('UPDATE users SET first_name = ?, last_name = ?, username = ?, password = ? WHERE id = ?',
 			[firstName, lastName, username, password, id]);
 	}
 
-	public async deleteAccount(id: number) {
+	public async deleteAccount(id: number): Promise<[ResultSetHeader, FieldPacket[]]> {
 		return (await this.connection).execute<ResultSetHeader>('DELETE FROM users WHERE id = ?', [id]);
 	}
 }
